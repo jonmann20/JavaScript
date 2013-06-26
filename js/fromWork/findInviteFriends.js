b1014e380655a7fe8cf016ccba814f31daa73882
Jon Wiedmann <jonwiedmann@gmail.com>
2013-06-26 12:56:51 -0400

    a

---
+++ js/fromWork/findInviteFriends.js
@@ -0,0 +1,178 @@
+﻿FindInviteFriends = function () {
+    var that = null,
+        numFriends = 10,
+        alreadyCached = false;  // prevents multiple requests to cache 
+
+    /*
+        EFFECTS: removes the friends who use YearlyMe from the list of all facebook friends
+                 a - b
+    */
+    function setDifference(a, b) {  // TODO: should modify 'a' directly instead of constructing new array and copying/overwriting
+        var result = [];
+
+        var i = 0, n = a.length,
+            j = 0, m = b.length;
+
+        while (i != n && j != m) {
+            //console.log(a[i].id + ' ?= ' + b[j].Item2);
+
+            if (a[i].id < b[j].Item2) {
+                result.push({
+                    id: a[i].id,
+                    pic: a[i].pic
+                });
+                ++i;
+            }
+            else if (b[j].Item2 < a[i].id) {
+                ++j;
+            }
+            else { // equal
+                ++i;
+                ++j;
+            }
+        }
+
+        return result;
+    }
+
+    return {
+        init: function () {
+            if ($('#loginPanel').length > 0) {
+                jAlert('Must be logged in.');
+                return;
+            }
+
+            /*
+                suggestList: user's friends who are NOT on YearlyMe || all user's friends (if not cached)
+                    id: facbook ids
+                    pic: facebook profile pictures
+
+                dbList: user's friends who are on YearlyMe
+                    Item1: YearlyMe usernames
+                    Item2: facebook ids
+            */
+            FbClass.getFriends(numFriends, function (suggestList, uncached) {    // get user's friends
+                //console.log(suggestList);
+
+                FbClass.getDatabaseFbFriends(suggestList, function (dbList) {    // search database for YearlyMe friends
+                    //console.log(dbList);
+
+                    //----------- Filter YearlyMe friends ----------\\
+                    if (uncached && dbList.length > 0) {
+                        suggestList = setDifference(suggestList, dbList);
+                    }
+
+                    var topN = (dbList.length < numFriends) ? dbList.length : numFriends,
+                        sugN = (suggestList.length < numFriends) ? suggestList.length : numFriends,
+                        str = '<div class="addFriends noselect">\
+                                <header>\
+                                    <h1>Find or Invite Friends:</h1>\
+                                </header>\
+                                <div class="strap"></div>\
+                                <div class="main">\
+                                    <h2 class="ymHdr">Find Facebook Friends on YearlyMe:</h2>\
+                                    <div class="ymFriendWrap clearfix">\
+                        ';
+
+                    // on YearlyMe:
+                    for (var i = 0; i < dbList.length; ++i) {
+                        str += '<a class="ymFriend" href="' + yearlyMe.baseURL + dbList[i].Item1 + '" target="_blank">\
+                                    <img class="friendImg" src="' + FbClass.getPicByID(dbList[i].Item2) + '" />\
+                                    <span class="name">' + dbList[i].Item1 + '\
+                                </a>';
+                    }
+
+                    // Suggest to YearlyMe
+                    str += '</div><div class="hr"><hr /></div>\
+                            <h2>Invite Facebook Friends to YearlyMe:</h2>\
+                            <div class="suggestWrap">';
+
+                    // TODO: amortize creating/loading images by page (possible to request all pics at once??)
+                    // ?redirect=false to get the correct url???
+
+                    var maxN = (suggestList.length > 50) ? 50 : suggestList.length;
+
+                    for (var i = 0; i < maxN; ++i) {
+                        str += '<img class="friendImg" src="' + suggestList[i].pic + '" />';
+                    }
+
+                    //str += '<a href="#" class="button suggestFriends">Suggest Friends</a>';
+                    str += '</div>\
+                            </div>\
+                        </div>';
+
+
+                    //---------- open pop-up and setup new click events ----------\\
+                    $.colorbox({
+                        html: str,
+                        width: 961,
+                        maxWidth: 961,
+                        className: 'addFriendsWrap',
+                        //innerWidth: 970,
+                        //innerHeight: 475,
+                        onComplete: function () {
+
+                            $('.ymFriendWrap').bxSlider({
+                                slideWidth: 68,
+                                minSlide: topN,
+                                maxSlides: topN,
+                                slideMargin: 15,
+                                infiniteLoop: false,
+                                prevSelector: '.addFriends',
+                                nextSelector: '.addFriends',
+                                hideControlOnEnd: true,
+                                pager: false,
+                                fbImg: true // custom option
+                            });
+
+                            $('.suggestWrap').bxSlider({
+                                slideWidth: 68,
+                                minSlide: sugN,
+                                maxSlides: sugN,
+                                slideMargin: 15,
+                                infiniteLoop: false,
+                                prevSelector: '.addFriends',
+                                nextSelector: '.addFriends',
+                                hideControlOnEnd: true,
+                                //pager: false,
+                                fbImg: true // custom option
+                            });
+
+                            $('.bx-wrapper').last().addClass('last');
+                            $('.bx-prev').last().css({ top: 290 });
+                            $('.bx-next').last().css({ top: 290 });
+                            $('.bx-prev, .bx-next').addClass('spriteAl');
+
+                            $(document).on('click', '.suggestWrap .friendImg', function () {
+                                var id = $(this).attr('src').split('/')[3];
+
+                                var url = 'http://www.facebook.com/dialog/feed?app_id=' + yearlyMe.fbAppId +
+                                          '&link=' + yearlyMe.baseURL + "signup.aspx?withFb=true&fromShare=true"; //yearlyMe.baseURL + yearlyMe.user.name;
+
+                                FbClass.getFbUsername(function (name) {
+                                   url += '&name=' + encodeURIComponent(name + ' has invited you to join YearlyMe!') +
+                                          '&description=' + encodeURIComponent("Join YearlyMe to create a collection of photos of yourself from each year of your life on YearlyMe.com.") +
+                                          '&redirect_uri=' + yearlyMe.baseURL + 'PopupClose.aspx' +
+                                          '&display=popup' +
+                                          '&to=' + id;
+
+                                   socialSharing.openWindow(url);
+                                });
+                            });
+                        }
+                    });
+
+                    if (dbList.length == 0)
+                        $('.ymHdr').html('You have no facebook friends on YearlyMe');
+
+                    //---- cache facebook friends
+                    if (!alreadyCached) {
+                        FbClass.storeSuggestFriends(suggestList);
+                        alreadyCached = true;
+                    }
+
+                }); // end getDbFriends
+            }); // end getFriends
+        }
+    }
+}();
---
+++ js/fromWork/viewPrint.js
@@ -0,0 +1,158 @@
+﻿var viewPrint = {
+    main: function () {
+        var sectionArr = $('.sectionBlock');
+        var VISIBLE_SECTIONS = sectionArr.length;
+
+        // calculate number of rows and cols
+        var NUM_ROWS = -1;
+        var NUM_COLS = [];
+
+        for (var i = 0; i < 3; i++)	// initialize NUM_COLS
+            NUM_COLS[i] = -1;
+
+        if (VISIBLE_SECTIONS < 3) {
+            NUM_ROWS = 1;
+            NUM_COLS[0] = VISIBLE_SECTIONS;
+        }
+
+        if (VISIBLE_SECTIONS > 2 && VISIBLE_SECTIONS <= 6) {
+            NUM_ROWS = 2;
+            NUM_COLS[0] = 3;
+            NUM_COLS[1] = VISIBLE_SECTIONS - 3;
+        }
+        else if (VISIBLE_SECTIONS > 6) {
+            NUM_ROWS = 3;
+            NUM_COLS[0] = 3;
+            NUM_COLS[1] = 3;
+            NUM_COLS[2] = VISIBLE_SECTIONS - 6;
+        }
+
+        //test NUM_COLS
+        for (var i = 0; i < 3; i++) {
+            //console.log('NUM_COLS[' + i + ']: ' + NUM_COLS[i]);
+        }
+
+        this.setup(VISIBLE_SECTIONS, sectionArr, NUM_ROWS, NUM_COLS);
+        this.autoHeight(VISIBLE_SECTIONS, sectionArr, NUM_ROWS, NUM_COLS);
+
+       
+    },
+
+    // add classes (row + number) to specify which row a group is in
+    setup: function (VISIBLE_SECTIONS, sectionArr, NUM_ROWS, NUM_COLS) {
+        //console.log('visible sections: ' + VISIBLE_SECTIONS);
+
+        var alreadyIterated = 3;
+
+        for (var i = 0; i < VISIBLE_SECTIONS; i++) {
+            var rowNum = 0;
+
+            if (i > 2 && i <= 5) {
+                rowNum = 1;
+            }
+            else if (i > 5) {
+                rowNum = 2;
+            }
+
+            var endOfRow = (rowNum * 3) + NUM_COLS[rowNum] - 1;
+
+            //console.log('i: ' + i);
+            //console.log('rowNum: ' + rowNum);
+            //console.log('endOfRow: ' + endOfRow);
+            //console.log(' ');
+
+            if (rowNum != alreadyIterated && endOfRow <= VISIBLE_SECTIONS) {
+                for (var j = i; j <= endOfRow; j++) {
+                    //console.log('j: ' + j);
+                    sectionArr.eq(j).addClass('row' + rowNum);
+                }
+
+                //console.log(' ');
+                alreadyIterated = rowNum;
+            }
+        }
+        
+    },
+
+    // calculate tallest section in each row
+    // set height of other sections in that row equal to the tallest section
+    // call calculatePageBreak()
+    autoHeight: function (VISIBLE_SECTIONS, sectionArr, NUM_ROWS, NUM_COLS) {
+        var rowArr = null;
+        var maxRowHeight = [NUM_ROWS];
+
+        // initialize maxRowHeight array
+        for (var i = 0; i < NUM_ROWS; i++) {
+            maxRowHeight[i] = 0;
+        }
+
+        // calculate tallest section in each row
+        for (var i = 0; i < NUM_ROWS; i++) {
+            rowArr = $('.row' + i);
+
+            for (var j = 0; j < NUM_COLS[i]; j++) {
+                var height = rowArr.eq(j).height();
+
+                if (height > maxRowHeight[i]) {
+                    maxRowHeight[i] = height;
+                }
+
+            }
+        }
+
+        // set height of other sections in each row equal to the tallest section
+        for (var i = 0; i < NUM_ROWS; i++) {
+            $('.row' + i).css({ height: maxRowHeight[i] });
+        }
+
+        // testing
+        for (var i = 0; i < NUM_ROWS; i++) {
+            rowArr = $('.row' + i);
+            //  console.log('Row ' + i + ': ' + maxRowHeight[i]);
+
+            // test individual sections
+            for (var j = 0; j < NUM_COLS[i]; j++) {
+                var height = rowArr.eq(j).height();
+
+                // console.log('    section ' + (j + i * 3) + ': ' + height);
+            }
+        }
+
+        
+        this.calcTotalHeight(maxRowHeight);
+    }, // end autoHeight
+
+    // add up column height
+    // if column height is greater than default page height force page break where appropriate
+    calcTotalHeight: function (maxRowHeight) {
+        var MAX_PAGE_HEIGHT = 855;
+        var totalHeight = 0;
+        var breakBeforeRow = 0;
+
+        for (var i = 0; i < maxRowHeight.length; i++) {
+            totalHeight += maxRowHeight[i];
+
+            if (totalHeight > MAX_PAGE_HEIGHT) {
+                breakBeforeRow = i;
+                break;
+            }
+        }
+
+        // console.log('totalHeight: ' + totalHeight);
+        // console.log('breakBeforeRow: ' + breakBeforeRow);
+
+        if (totalHeight > MAX_PAGE_HEIGHT) {
+            $('.row' + breakBeforeRow).eq(0).before("<div class='pageBreak'>&nbsp;</div>");
+        }
+   
+        if (printBtnClicked == true) {
+            window.print();
+            window.close();
+        }
+
+    } // end calcTotalHeight  
+};     // end viewPrint
+
+$(function () {
+	viewPrint.main();
+});
